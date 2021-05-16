80cd80047b05fabd92f6d55e670b5eb1604c862b
hibasalem <salemhiba.hs@gmail.com>
2021-05-16 23:16:47 +0300

    initial solve

---
+++ src/components/Footer .js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>
+                Author : Hiba Salem 
+                </p>
+
+            </div>
+        )
+    }
+}
+export default Footer;
