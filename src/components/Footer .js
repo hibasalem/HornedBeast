77273a620e18bfa643de5a0375fe686c026aade9
hibasalem <salemhiba.hs@gmail.com>
2021-05-17 01:00:42 +0300

    edited main

--- src/components/Footer .js
+++ src/components/Footer .js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 class Footer extends React.Component{
+
     render(){
         return(
             <div>
+                <footer>
                 <p>
                 Author : Hiba Salem 
                 </p>
-
+                </footer>
             </div>
         )
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-class Header extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                Horned Beasts
-                </h1>
+class Header extends React.Component {
+    render() {
 
+        return (
+            <div>
+                <header>
+                    <h1>
+                        Horned Beasts
+                </h1>
+                </header>
             </div>
         )
     }
